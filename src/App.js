8ffaf889032ba42820d599f992d9b5e69f6f2f97
Stepan Suvorov <stevermeister@gmail.com>
2023-01-03 17:51:24 +0100

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {  
+    return (
+      <div className="container">
+        <h1>Year progress</h1>
+        <div className="progressbar-container">
+          <div className="progressbar-complete" style={{width: `${getProgress()}%`}}>
+            <div className="progressbar-liquid"></div>
+          </div>
+          <span className="progress">{dayOfYear()}/365</span>
+        </div>
+      </div>
+    )
+}
+
+function getProgress() {
+  const now = new Date().getTime()
+  const firstDayOfYear = new Date(new Date().getFullYear(), 0, 1).getTime()
+  const firstDayOfNextYear = new Date(new Date().getFullYear() + 1, 0, 1).getTime()
+  return  Math.round((now - firstDayOfYear) / (firstDayOfNextYear - firstDayOfYear) * 100);
+}
+
+function dayOfYear() {
+  const today = new Date();
+  const year = today.getFullYear();
+
+  // Create a new date object for the start of the year
+  const startOfYear = new Date(year, 0, 1);
+
+  // Calculate the number of milliseconds between the start of the year and the current date
+  const millisecondsSinceStartOfYear = today - startOfYear;
+
+  // Convert the number of milliseconds to the number of days
+  const dayOfYear = Math.floor(millisecondsSinceStartOfYear / (1000 * 60 * 60 * 24));
+
+  return dayOfYear;
 }
 
 export default App;
