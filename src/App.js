82eb73bc1de77c25e21cb4bb47fc30f199375eca
Stepan Suvorov <stevermeister@gmail.com>
2023-01-04 18:25:55 +0100

    more goals

--- src/App.js
+++ src/App.js
@@ -2,16 +2,48 @@ import './App.scss';
 import ProgressBar from './ProgressBar';
 
 function App() {  
+
+  const goals = [
+    { title: 'Visit a new country', start: 0, goal: 2},
+    { title: 'Visit a new city', start: 0, goal: 20},
+    { title: 'Swim in the sea', start: 0, goal: 2},
+    { title: 'Read book', start: 0, goal: 25},
+    { title: 'Listen audiobook', start: 0, goal: 10},
+    { title: 'Watch film', start: 0, goal: 20},
+    { title: 'Get weight', start: 65, goal: 75},
+    { title: 'Conference talk', start: 0, goal: 4},
+    { title: 'Angular Meetup', start: 0, goal: 3},
+    { title: 'Chess raiting', start: 1800, goal: 2100},
+    { title: 'Gym', start: 0, goal: 100},
+  ];
+
+  const statusNumber = {
+    'Visit a new country' : 0,
+    'Visit a new city' : 0,
+    'Swim in the sea' : 0,
+    'Read book' : 0,
+    'Listen audiobook' : 0,
+    'Watch film' : 0,
+    'Get weight' : 65,
+    'Conference talk' : 0,
+    'Angular Meetup' : 0,
+    'Chess raiting' : 1850,
+    'Gym' : 2 
+  }
+  
+
+
     return (
       <div className="container">
         <h1>Goals progress</h1>
-        
 
         <ul>
-          <li>Year: <ProgressBar total={365} number={dayOfYear()}/></li>
-          <li>Gym: <ProgressBar total={100} number={2}/></li>
-          <li>Films: <ProgressBar total={20} number={0}/></li>
+          <li>Year: <ProgressBar start={0} total={365} number={dayOfYear()}/></li>
+          {goals.map((goal, index) => (
+            <li>{goal.title}: <ProgressBar start={goal.start} total={goal.goal} number={statusNumber[goal.title]}/></li>
+          ))}
         </ul>
+
       </div>
     )
 }
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -4,7 +4,7 @@ function ProgressBar(props) {
 
   return (
       <div className="progressbar-container">
-        <div className="progressbar-complete" style={{ width: `${Math.floor(props.number/props.total*100)}%` }}>
+        <div className="progressbar-complete" style={{ width: `${Math.floor((props.number-props.start)/props.total*100)}%` }}>
           <div className="progressbar-liquid"></div>
         </div>
         <span className="progress">{props.number}/{props.total}</span>
