d8c0ba610bed59589cb12d58edf25ee70918c1d6
Stepan Suvorov <stevermeister@gmail.com>
2025-01-15 19:37:22 +0100

    bin instead of cross

--- src/pages/Goals.tsx
+++ src/pages/Goals.tsx
@@ -223,7 +223,9 @@ export function Goals() {
                         }}
                         title="Remove goal"
                       >
-                        ×
+                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                          <path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2M10 11v6M14 11v6" />
+                        </svg>
                       </button>
                     </div>
                   </li>
